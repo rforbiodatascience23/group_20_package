c6d013d797c032d6c490dec911d4bcc30a58500d
line-dh <s184242@student.dtu.dk>
2023-10-24 10:05:33 +0200

    codon_table update

---
+++ R/data.R
@@ -0,0 +1,8 @@
+#' Title
+#'
+#' Description
+#'
+#' A table that translates codons into amino acids.
+#'
+#' @source \url{https://www.ncbi.nlm.nih.gov/Taxonomy/Utils/wprintgc.cgi?chapter=tgencodes#SG1}
+"codon_table"
